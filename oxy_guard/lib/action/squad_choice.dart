41c4699b319f0cfc6ea6e36bf88faef55d3f137f
Adam Łukaszewicz <jakub.adam20@gmail.com>
2024-10-18 18:50:57 +0200

    Action and squad choice pages redesigns

--- oxy_guard/lib/action/squad_choice.dart
+++ oxy_guard/lib/action/squad_choice.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:oxy_guard/action/manage_page.dart';
 import 'package:oxy_guard/global_service.dart';
 import 'package:oxy_guard/home_page.dart';
@@ -13,21 +14,24 @@ class SquadChoice extends StatefulWidget {
 }
 
 class _SquadChoiceState extends State<SquadChoice> {
-
   @override
   void initState() {
     super.initState();
-    if(widget.quickStart){
+    if (widget.quickStart) {
       widget.quickStart = false;
       WidgetsBinding.instance.addPostFrameCallback((_) {
         Navigator.push(
           context,
-          MaterialPageRoute(builder: (context) => ManagePage(chosenAction: GlobalService.currentAction.squads["0"], quickStart: true,)),
+          MaterialPageRoute(
+              builder: (context) => ManagePage(
+                    chosenAction: GlobalService.currentAction.squads["0"],
+                    quickStart: true,
+                  )),
         );
       });
-    }else if (GlobalService.currentAction.squads.entries.toList().isEmpty) {
+    } else if (GlobalService.currentAction.squads.entries.toList().isEmpty) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-         Navigator.push(
+        Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => ManagePage()),
         );
@@ -39,62 +43,183 @@ class _SquadChoiceState extends State<SquadChoice> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Center(child: Text("OxyGuard")),
-      ),
+          backgroundColor: Theme.of(context).primaryColorDark,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          title: const Text("Akcja")),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all(
-                  Size(MediaQuery.of(context).size.width * 0.7, 0)
-                ),
-              ),
-              onPressed: () async {
-                String? chosenSquadIndex = await chooseExistingSquadDialog();
-                if (chosenSquadIndex != null) {
-                  SquadModel rebuild = SquadModel();
-                  rebuild.copyFrom(GlobalService.currentAction.squads[chosenSquadIndex]!);
-                  GlobalService.currentAction.squads[chosenSquadIndex] = rebuild;
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => ManagePage(
-                              chosenAction: rebuild,
-                            )),
-                  );
-                }
-              },
-              child: const Center(child: Text("Wybierz odcinek"))),
-            SizedBox(height: 13),
-            ElevatedButton(
-              style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all(
-                  Size(MediaQuery.of(context).size.width * 0.7, 0)
-                ),
-              ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ManagePage()),
-                  );
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.20,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  return Card(
+                  color: Colors.white,
+                  elevation: 5,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: constraints.maxHeight * 0.02, horizontal: constraints.maxWidth * 0.05),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: constraints.maxHeight * 0.1,),
+                        const Text(
+                          "Miejsce akcji:",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        SizedBox(height: constraints.maxHeight * 0.1,),
+                        FutureBuilder(
+                          future: placemarkFromCoordinates(
+                              GlobalService.currentAction.actionLocation.latitude,
+                              GlobalService
+                                  .currentAction.actionLocation.longitude),
+                          builder: (context, snap) {
+                            if (snap.connectionState == ConnectionState.done) {
+                              if (snap.hasData) {
+                                final address = "${snap.data!.first.street}";
+                                final city = "${snap.data!.first.locality}";
+                                return Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      city,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 28),
+                                    ),
+                                    SizedBox(height: constraints.maxHeight * 0.05,),
+                                    Text(
+                                      address,
+                                      style: const TextStyle(
+                                        fontSize: 24,
+                                      ),
+                                    )
+                                  ],
+                                );
+                                //MOŻLIWY BUG: nie wiem czy ta logika pokryje brak lokalizacji w akcji (czy brak actionlocation spowoduje snap.hasError czy cos co nie jest handlowane)
+                              } else if (snap.hasError) {
+                                return const ListTile(
+                                  title:
+                                      Text("Brak pasującego adresu/nazwy akcji"),
+                                );
+                              }
+                            }
+                            return const ListTile(
+                              title: Text("Ładowanie..."),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
                 },
-                child: const Center(child: Text("Stwórz odcinek"))),
-            SizedBox(height: 13),
-            ElevatedButton(
-              style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all(
-                  Size(MediaQuery.of(context).size.width * 0.7, 0)
-                ),
               ),
-              onPressed: () {
-                GlobalService.databaseSevice.endAction(GlobalService.currentAction);
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
-              },
-            child: const Center(child: Text("Zakończ akcję"))),
-      ],
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.10),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                    style: ButtonStyle(
+                      fixedSize: WidgetStateProperty.all(Size(
+                          MediaQuery.of(context).size.width * 0.7,
+                          MediaQuery.of(context).size.height * 0.07)),
+                      shape: WidgetStateProperty.all(
+                          const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                              side: BorderSide(width: 0.1))),
+                      elevation: const WidgetStatePropertyAll(5),
+                      backgroundColor:
+                          const WidgetStatePropertyAll(Colors.white),
+                    ),
+                    onPressed: () async {
+                      String? chosenSquadIndex =
+                          await chooseExistingSquadDialog();
+                      if (chosenSquadIndex != null) {
+                        SquadModel rebuild = SquadModel();
+                        rebuild.copyFrom(GlobalService
+                            .currentAction.squads[chosenSquadIndex]!);
+                        GlobalService.currentAction.squads[chosenSquadIndex] =
+                            rebuild;
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ManagePage(
+                                    chosenAction: rebuild,
+                                  )),
+                        );
+                      }
+                    },
+                    child: Text(
+                      "Wybierz odcinek",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColorDark,
+                          fontSize: 24),
+                    )),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                ElevatedButton(
+                    style: ButtonStyle(
+                      fixedSize: WidgetStateProperty.all(Size(
+                          MediaQuery.of(context).size.width * 0.7,
+                          MediaQuery.of(context).size.height * 0.07)),
+                      shape: WidgetStateProperty.all(
+                          const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                              side: BorderSide(width: 0.1))),
+                      elevation: const WidgetStatePropertyAll(5),
+                      backgroundColor:
+                          const WidgetStatePropertyAll(Colors.white),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ManagePage()),
+                      );
+                    },
+                    child: Text(
+                      "Stwórz odcinek",
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColorDark,
+                          fontSize: 24),
+                    )),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+                ElevatedButton(
+                    style: ButtonStyle(
+                      fixedSize: WidgetStateProperty.all(Size(
+                          MediaQuery.of(context).size.width * 0.7,
+                          MediaQuery.of(context).size.height * 0.07)),
+                      shape: WidgetStateProperty.all(
+                          const RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15)),
+                              side: BorderSide(width: 0.1))),
+                      elevation: const WidgetStatePropertyAll(5),
+                      backgroundColor: const WidgetStatePropertyAll(Colors.red),
+                    ),
+                    onPressed: () {
+                      GlobalService.databaseSevice
+                          .endAction(GlobalService.currentAction);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()));
+                    },
+                    child: const Text(
+                      "Zakończ akcję",
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    )),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- oxy_guard/lib/action_list.dart
+++ oxy_guard/lib/action_list.dart
@@ -8,7 +8,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:oxy_guard/action/squad_choice.dart';
 import 'package:oxy_guard/models/action_model.dart';
 
-
 import 'global_service.dart';
 
 class ActionList extends StatefulWidget {
@@ -24,10 +23,10 @@ class _ActionListState extends State<ActionList> {
     try {
       placemark = placemarkFromCoordinates(latitude, longitude);
       return placemark;
-    }on PlatformException catch(err){
+    } on PlatformException catch (err) {
       print("Caught the odd one");
       return Future.error(err);
-    }catch (err) {
+    } catch (err) {
       return Future.error(err);
     }
   }
@@ -35,98 +34,153 @@ class _ActionListState extends State<ActionList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Dołącz do trwającej akcji"),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).primaryColorDark,
+        foregroundColor: Colors.white,
+      ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: StreamBuilder(
-                  stream: GlobalService.databaseSevice.getActions(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return const Text('Something went wrong');
-                    }
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.05,
+              vertical: MediaQuery.of(context).size.height * 0.02),
+          child: Column(
+            children: [
+              Expanded(
+                child: StreamBuilder(
+                    stream: GlobalService.databaseSevice.getActions(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasError) {
+                        return const Text('Something went wrong');
+                      }
 
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Text("Loading");
-                    }
-                    List<(Position, Card)> preSort = snapshot.data!.docs.map((action) {      
-                      Position actionLocation = Position.fromMap(jsonDecode(action["Location"]!));
-                      return (
-                        actionLocation,
-                        Card(
-                            child: InkWell(
-                          child: ListTile(
-                            title: (action.data() as ActionModel).actionName.isNotEmpty ? Text((action.data() as ActionModel).actionName) : FutureBuilder(
-                              future: placemarkFromCoordinates(actionLocation.latitude, actionLocation.longitude),
-                              builder: (context, snap) {
-                                if (snap.connectionState == ConnectionState.done) {
-                                  if (snap.hasData) {
-                                    final address = "${snap.data!.first.street}, ${snap.data!.first.locality}";
-                                    return Text(address);
-                                  } else if (snap.hasError) {
-                                    return const Text("Brak pasującego adresu");
-                                  }
-                                }
-                                return const Text("Loading...");
-                              },
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CircularProgressIndicator(),
+                              ],
                             ),
-                          ),
-                          onTap: () {
-                            GlobalService.databaseSevice.joinAction(action.id);
-                            GlobalService.currentAction =
-                                action.data() as ActionModel;
-                            GlobalService.currentAction.listenToChanges();
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SquadChoice()));
-                          },
-                        ))
-                      );
-                    }).toList();
-                    if (GlobalService.permission == LocationPermission.always ||
-                        GlobalService.permission ==
-                            LocationPermission.whileInUse) {
-                      return FutureBuilder(
-                        future: Geolocator.getCurrentPosition(),
-                        builder: (context, snap) {
-                          if (snap.connectionState == ConnectionState.done) {
-                            if (snap.hasData) {
-                              Position currentPostion = snap.data!;
-                              preSort.sort((a, b) {
-                                double distanceToA = Geolocator.distanceBetween(
-                                    currentPostion.latitude,
-                                    currentPostion.longitude,
-                                    a.$1.latitude,
-                                    a.$1.longitude);
-                                double distanceToB = Geolocator.distanceBetween(
-                                    currentPostion.latitude,
-                                    currentPostion.longitude,
-                                    b.$1.latitude,
-                                    b.$1.longitude);
-                                return distanceToA.compareTo(distanceToB);
-                              });
-                              return ListView(
-                                children: preSort.map((e) => e.$2).toList(),
-                              );
+                          ],
+                        );
+                      }
+                      List<(Position, Card)> preSort =
+                          snapshot.data!.docs.map((action) {
+                        Position actionLocation =
+                            Position.fromMap(jsonDecode(action["Location"]!));
+                        return (
+                          actionLocation,
+                          Card(
+                            color: Colors.white,
+                            elevation: 5,
+                              child: InkWell(
+                            child: FutureBuilder(
+                                      future: placemarkFromCoordinates(
+                                          actionLocation.latitude,
+                                          actionLocation.longitude),
+                                      builder: (context, snap) {
+                                        if (snap.connectionState ==
+                                            ConnectionState.done) {
+                                          if (snap.hasData) {
+                                            final address = "${snap.data!.first.street}";
+                                            final city = "${snap.data!.first.locality}";
+                                            return ListTile(
+                                              title: Text(
+                                                city,
+                                                style: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 24
+                                                ),
+                                                ),
+                                              subtitle: Text(
+                                                address,
+                                                style: const TextStyle(
+                                                  fontSize: 18,
+                                                ),
+                                                ),
+                                              //MOŻLIWY BUG: nie wiem czy ta logika pokryje brak lokalizacji w akcji (czy brak actionlocation spowoduje snap.hasError czy cos co nie jest handlowane)
+                                            );
+                                          } else if (snap.hasError) {
+                                            return const ListTile(
+                                              title: Text("Brak pasującego adresu/nazwy akcji"),
+                                            );
+                                          }
+                                        }
+                                        return const ListTile(
+                                          title: Text("Ładowanie..."),
+                                        );
+                                      },
+                                    ),
+                            onTap: () {
+                              GlobalService.databaseSevice
+                                  .joinAction(action.id);
+                              GlobalService.currentAction =
+                                  action.data() as ActionModel;
+                              GlobalService.currentAction.listenToChanges();
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SquadChoice()));
+                            },
+                          ))
+                        );
+                      }).toList();
+                      if (GlobalService.permission ==
+                              LocationPermission.always ||
+                          GlobalService.permission ==
+                              LocationPermission.whileInUse) {
+                        return FutureBuilder(
+                          future: Geolocator.getCurrentPosition(),
+                          builder: (context, snap) {
+                            if (snap.connectionState == ConnectionState.done) {
+                              if (snap.hasData) {
+                                Position currentPostion = snap.data!;
+                                preSort.sort((a, b) {
+                                  double distanceToA =
+                                      Geolocator.distanceBetween(
+                                          currentPostion.latitude,
+                                          currentPostion.longitude,
+                                          a.$1.latitude,
+                                          a.$1.longitude);
+                                  double distanceToB =
+                                      Geolocator.distanceBetween(
+                                          currentPostion.latitude,
+                                          currentPostion.longitude,
+                                          b.$1.latitude,
+                                          b.$1.longitude);
+                                  return distanceToA.compareTo(distanceToB);
+                                });
+                                return ListView(
+                                  children: preSort.map((e) => e.$2).toList(),
+                                );
+                              }
                             }
-                          }
-                          return const Text("LOADING");
-                        },
-                      );
-                    } else {
-                      return ListView(
-                        children: preSort.map((e) => e.$2).toList(),
-                      );
-                    }
-                  }),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text("Wróć")),
-          ],
+                            return const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    CircularProgressIndicator(),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      } else {
+                        return ListView(
+                          children: preSort.map((e) => e.$2).toList(),
+                        );
+                      }
+                    }),
+              ),
+            ],
+          ),
         ),
       ),
     );
